f6cf360e7be439e328f183aae64a7cf9dfd2536b
Sokol499 <80357235+Sokol499@users.noreply.github.com>
2024-09-25 12:49:20 +0300

    Create bot_cmds_list.py

---
+++ TestBot/common/bot_cmds_list.py
@@ -0,0 +1,9 @@
+from aiogram.types import BotCommand
+
+private = [
+    BotCommand(command='history', description='История клуба'),
+    BotCommand(command='players', description='Состав команды'),
+    BotCommand(command='titles', description='Значимые титулы'),
+    BotCommand(command='statistics', description='Показатели игроков'),
+    BotCommand(command='admin', description='Для админа')
+]

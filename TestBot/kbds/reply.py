7c75c7913edf54e8cbde2d270adbc4e3a62921e3
Sokol499 <80357235+Sokol499@users.noreply.github.com>
2024-09-25 12:51:10 +0300

    Create reply.py

---
+++ TestBot/kbds/reply.py
@@ -0,0 +1,16 @@
+from aiogram.types import KeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+
+def get_keyboard(
+        *btns: str,
+        placeholder: str = None,
+        sizes: tuple[int] = (2,),
+):
+    keyboard = ReplyKeyboardBuilder()
+
+    for index, text in enumerate(btns, start=0):
+        keyboard.add(KeyboardButton(text=text))
+
+    return keyboard.adjust(*sizes).as_markup(
+        resize_keyboard=True, input_field_placeholder=placeholder)
